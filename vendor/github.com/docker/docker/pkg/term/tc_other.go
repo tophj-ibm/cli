5112d4919d8a4b3618ff46aa366a359de47f8f29
Christopher Jones <tophj@linux.vnet.ibm.com>
2017-05-04 17:28:06 -0400

    updated cgo

--- vendor/github.com/docker/docker/pkg/term/tc_other.go
+++ vendor/github.com/docker/docker/pkg/term/tc_other.go
@@ -5,16 +5,17 @@
 package term
 
 import (
-	"syscall"
 	"unsafe"
+	"syscall"
+	"golang.org/x/sys/unix"
 )
 
 func tcget(fd uintptr, p *Termios) syscall.Errno {
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, uintptr(getTermios), uintptr(unsafe.Pointer(p)))
+	_, _, err := unix.Syscall(unix.SYS_IOCTL, fd, uintptr(getTermios), uintptr(unsafe.Pointer(p)))
 	return err
 }
 
 func tcset(fd uintptr, p *Termios) syscall.Errno {
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, setTermios, uintptr(unsafe.Pointer(p)))
+	_, _, err := unix.Syscall(unix.SYS_IOCTL, fd, setTermios, uintptr(unsafe.Pointer(p)))
 	return err
 }
