b8c2cbee39eebb74d449b2b68078300cbeff1ea4
Christy Perez <christy@linux.vnet.ibm.com>
2017-05-25 14:33:48 -0500

    Check for existing transaction on create
    If exists, don't continue, unless the user wants to amend the
    transaction.
    
    Adding the -a/--amend flag to `create`.
    
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/command/manifest/create_list.go
+++ cli/command/manifest/create_list.go
@@ -12,21 +12,29 @@ import (
 	"github.com/docker/docker/registry"
 )
 
+type annotateOpts struct {
+	amend bool
+}
+
 func newCreateListCommand(dockerCli *command.DockerCli) *cobra.Command {
+
+	opts := annotateOpts{}
 
 	cmd := &cobra.Command{
 		Use:   "create newRef manifest [manifest...]",
 		Short: "Create a local manifest list for annotating and pushing to a registry",
 		Args:  cli.RequiresMinArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return createManifestList(dockerCli, args)
+			return createManifestList(dockerCli, args, opts)
 		},
 	}
 
+	flags := cmd.Flags()
+	flags.BoolVarP(&opts.amend, "amend", "a", false, "Amend an existing manifest list transaction")
 	return cmd
 }
 
-func createManifestList(dockerCli *command.DockerCli, args []string) error {
+func createManifestList(dockerCli *command.DockerCli, args []string, opts annotateOpts) error {
 
 	// Just do some basic verification here, and leave the rest for when the user pushes the list
 	newRef := args[0]
@@ -37,6 +45,18 @@ func createManifestList(dockerCli *command.DockerCli, args []string) error {
 	_, err = registry.ParseRepositoryInfo(targetRef)
 	if err != nil {
 		return fmt.Errorf("Error parsing repository name for manifest list (%s): %v", newRef, err)
+	}
+
+	// Check locally for this list transaction before proceeding
+	if _, isDigested := targetRef.(reference.Canonical); !isDigested {
+		targetRef = reference.TagNameOnly(targetRef)
+	}
+	manifestFiles, err := getListFilenames(makeFilesafeName(targetRef.String()))
+	if err != nil {
+		return err
+	}
+	if len(manifestFiles) > 0 && !opts.amend {
+		return fmt.Errorf("Refusing to continue over an existing manifest list transaction with no --amend flag")
 	}
 
 	// Now create the local manifest list transaction by looking up the manifest schemas
