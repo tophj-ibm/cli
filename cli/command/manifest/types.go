c0116dce7745d1308e230fa62e4b4a371fd4402b
Christy Perez <christy@linux.vnet.ibm.com>
2017-06-21 11:42:17 -0500

    Remove fallback to v1 registry endpoints paths
    @TODO: Still need to put in logic to try other enpoints in cases
    that manifest is not found or auth is rejected, etc.
    
    Signed-off-by: Christy Perez <christy@linux.vnet.ibm.com>

--- cli/command/manifest/types.go
+++ cli/command/manifest/types.go
@@ -5,23 +5,6 @@ import (
 	"github.com/opencontainers/go-digest"
 )
 
-// fallbackError wraps an error that can possibly allow fallback to a different
-// endpoint.
-type fallbackError struct {
-	// err is the error being wrapped.
-	err error
-	// confirmedV2 is set to true if it was confirmed that the registry
-	// supports the v2 protocol. This is used to limit fallbacks to the v1
-	// protocol.
-	confirmedV2 bool
-	transportOK bool
-}
-
-// Error renders the FallbackError as a string.
-func (f fallbackError) Error() string {
-	return f.err.Error()
-}
-
 // ImgManifestInspect contains info to output for a manifest object.
 type ImgManifestInspect struct {
 	RefName         string                 `json:"ref"`
